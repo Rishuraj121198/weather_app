8e01388fffbf1bd8ba000f8d31bc089965bf48fa
Rish Raj <rishuraj121198@gmail.com>
2024-01-13 01:59:43 +0530

    first commit

---
+++ src/component/Weather.jsx
@@ -0,0 +1,44 @@
+// src/components/WeatherApp.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const WeatherApp = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const apiKey = "a570f2a48fcbc990f3f05ce3abdf1ffc";
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={getWeatherData}>Get Weather</button>
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Weather: {weatherData.weather[0].description}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
